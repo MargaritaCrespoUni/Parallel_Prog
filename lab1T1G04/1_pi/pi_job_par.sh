aebd928562b2d31b125fd8d3d1c2de31311026e4
Margarita Crespo Villaverde <margarita.crespo01@estudiant.upf.edu>
2024-04-18 18:10:17 +0200

    Second session

--- lab1T1G04/1_pi/pi_job_par.sh
+++ lab1T1G04/1_pi/pi_job_par.sh
@@ -4,16 +4,13 @@
 #SBATCH -p ippd-cpu
 #SBATCH --output=pi_%j.out
 #SBATCH --error=pi_%j.err
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --time=00:00:30
 
 #Compile the binary using Makefile
-make seq || exit 1 #exit if it doesn't work 
+make seq || exit 1 #exit if it doesn't work
 
 #Execute the binary that we compiled earlier
-./pi_seq 10
-./pi_seq 10000
-./pi_seq 1000000
-./pi_seq 10000000000
+./pi_par 10
---
+++ lab1T1G04/1_pi/pi_par.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+double compute_pi(int N) {
+    double sum = 0.0;
+    double delta_x = 1.0 / N;
+    double x;
+    double height;
+    #pragma omp parallel
+    {
+        for (int i = 0; i < N; i++) {
+            x = (i + 0.5) / N;
+            height = 4 / (1 + x * x);
+            sum += delta_x * height;
+        }
+    }
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+    int N = atoi(argv[1]);
+    clock_t start_time = clock();
+    double pi = compute_pi(N);
+    clock_t end_time = clock();
+
+    double runtime = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("\nPi with %i steps is %.15lf in %lf seconds\n", N, pi, runtime);
+}
